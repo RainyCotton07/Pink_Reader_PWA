7a1b0647cccb8fda1d2b2cc8cb8a57c571b2dccf
Rainycotton07 <yafujitterrx784@gmail.com>
2025-08-05 09:49:49 +0900

    オフライン利用永続化

--- sw.js
+++ sw.js
@@ -15,7 +15,15 @@ const CORE_FILES = [
   './js/image-viewer.js',
   './js/video-player.js',
   './js/utils.js',
+  './icons/app_icon.png',
+  './icons/icon-32x32.png',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
   './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
   './icons/icon-512x512.png'
 ];
 
